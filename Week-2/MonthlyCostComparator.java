83b55e96941c62a85e2d552818efc6c76d38d779
Shikhar Padhy <111035966+Rikshik@users.noreply.github.com>
2022-08-13 14:59:28 +0530

    Delete MonthlyCostComparator.java

--- Week-2/MonthlyCostComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class MonthlyCostComparator implements Comparator<DB>{
-	public int compare(DB db1, DB db2) {
-		return (int) (db1.BaseCost - db2.BaseCost);
-	}
-}
-
